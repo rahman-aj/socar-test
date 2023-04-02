0ec134e81aac4e657d6410eb47a6f16da38803cd
rahman <rahman.aboujeeb@gmail.com>
2023-04-02 16:32:05 +0800

    Setup tab bar

--- navigation/mainContainer.js
+++ navigation/mainContainer.js
@@ -8,6 +8,9 @@ import { Store } from '../redux/store';
 
 // Screens
 import HomeScreen from './screens/homeScreen';
+import BookingScreen from './screens/bookingScreen';
+import ProfileScreen from './screens/profileScreen';
+import SettingsScreen from './screens/settingsScreen';
 
 const Tab = createBottomTabNavigator()
 
@@ -23,6 +26,12 @@ export default function MainContainer() {
 
                             if (route.name === 'Home') {
                                 iconName = focused ? 'home' : 'home-outline'
+                            } else if (route.name === 'Booking') {
+                                iconName = focused ? 'car' : 'car-outline'
+                            } else if (route.name === 'Profile') {
+                                iconName = focused ? 'person' : 'person-outline'
+                            } else if (route.name === 'Settings') {
+                                iconName = focused ? 'settings' : 'settings-outline'
                             }
 
                             return <Ionicons name={iconName} size={size} color={color} />
@@ -30,7 +39,10 @@ export default function MainContainer() {
                         tabBarLabelStyle: { fontSize: 12 },
                         tabBarStyle: { paddingVertical: 8, height: 90 }
                     })}>
-                    <Tab.Screen name="Home" component={HomeScreen} />
+                    <Tab.Screen name='Home' component={HomeScreen} />
+                    <Tab.Screen name='Booking' component={BookingScreen} />
+                    <Tab.Screen name='Profile' component={ProfileScreen} />
+                    <Tab.Screen name='Settings' component={SettingsScreen} />
                 </Tab.Navigator>
             </NavigationContainer >
         </Provider>
---
+++ navigation/screens/profileScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ProfileScreen({ navigation }) {
+    return (
+        <View>
+           <Text>Profile Page!</Text>
+        </View>
+    )
+}
